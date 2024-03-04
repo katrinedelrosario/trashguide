3d01e1355014f8109ac8f7309ebdf7b29708eb41
katrinedelrosario <katrinedelrosario@gmail.com>
2024-03-04 18:24:55 +0100

    navbar setup

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,22 +1,33 @@
 import { NavLink } from "react-router-dom";
-//import Logo from "../../assets/logo.svg"
+import logo from "../../assets/images/layout/logo.svg"
 import style from "./navbar.module.scss"
-
+import { Title } from "../title/title";
+import lockIcon from '../../assets/images/layout/icon-unlock.svg';
+import ellipseBg from '../../assets/images/layout/green-ellipse.svg';
 export const Navbar = () => {
 
     
     return (
         <nav className={style.navbarStyle}>
             <div className={style.header}>
-                {/* <img src={Logo} alt="Hotel Overlook Logo" /> */}
+                <img src={logo} alt="Affaldsguiden Logo" />
+                <Title title='Affaldsguiden'/>
             </div>
 
             <ul>
-                <li><NavLink to="/frontpage">forside</NavLink></li>
-                <li><NavLink to="/1">1</NavLink></li>
-                <li><NavLink to="/2">2</NavLink></li>
-                <li><NavLink to="/login">login</NavLink></li>
+                <li><NavLink to="/frontpage">Forside</NavLink></li>
+                <li><NavLink to="/sorting">Sortering</NavLink></li>
+                <li><NavLink to="/stations">Genbrugsstationer</NavLink></li>
+                <li><NavLink to="/order-container">Bestil beholder</NavLink></li>
+                
             </ul>
+
+            <div className={style.loginBtn}>
+                <li><NavLink to="/login">
+                        <img src={ellipseBg} className={style.ellipse} alt="ellipse background"/>
+                        <img src={lockIcon} className={style.lockIcon} alt="lock icon" />
+                </NavLink></li>
+            </div>
         </nav>
     )
 }
--- src/components/title/title.jsx
+++ src/components/title/title.jsx
@@ -1,5 +1,5 @@
 import style from "./title.module.scss"
 
 export const Title = ({title}) => {
-    return <h2 className={style.titleStyle}>{title}</h2>
+    return <h1 className={style.title}>{title}</h1>
 }
