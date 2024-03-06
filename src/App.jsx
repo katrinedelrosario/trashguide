f9bbae11072ffa197cd6bff2f3f5bff0c411e830
katrinedelrosario <katrinedelrosario@gmail.com>
2024-03-06 13:30:37 +0100

    other

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { MainLayout } from './layout/mainLayout'
 import { Frontpage } from './pages/frontpage/frontpage'
 import { Login } from './pages/login/login'
+import { Sortingpage } from './pages/sortingpage/sortingpage';
+import { Stationspage } from './pages/stationspage/stationspage';
+import { Orderpage } from './pages/orderpage/orderpage';
 
 function App() {
   return (
@@ -11,7 +14,10 @@ function App() {
         <Routes>
           <Route path='/' element={<MainLayout />}>
           <Route index element={<Frontpage />} />
-          <Route path='/forside' element={<Frontpage />} />
+          <Route path='/frontpage' element={<Frontpage />} />
+          <Route path='/sorting' element={<Sortingpage/>} />
+          <Route path='/stations' element={<Stationspage />} />
+          <Route path='/order' element={<Orderpage />} />
           <Route path='/login' element={<Login />} />
           </Route>
         </Routes>
