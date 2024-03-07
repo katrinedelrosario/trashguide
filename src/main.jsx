ba063f34cb4f977bbbd31e5bad66a06918b575b2
katrinedelrosario <katrinedelrosario@gmail.com>
2024-03-07 12:28:23 +0100

    added providers

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import '../index.css'
+import { SectionProvider } from './providers/sectionProvider.jsx'
+import { AuthProvider } from './providers/useAuth.jsx'
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+    <SectionProvider>
+      <App />
+    </SectionProvider>
+    </AuthProvider>
   </React.StrictMode>,
 )
